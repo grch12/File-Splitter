f4be4e804e64a68cc53af20fac2fc06d0669b4ad
grch <grch@DESKTOP-38G2HV1>
2023-01-27 17:56:34 +0800

    现在可以通过指定大小来分割文件

--- File Splitter/File Splitter.cpp
+++ File Splitter/File Splitter.cpp
@@ -1,10 +1,10 @@
-﻿#include <iostream>
-#include <fstream>
+﻿#include <fstream>
+#include <iostream>
 #include <string>
 
 using namespace std;
 
-size_t fileSize(ifstream &file)
+size_t fileSize(ifstream& file)
 {
     streampos fpos = file.tellg();
     file.seekg(0, ios_base::end);
@@ -20,7 +20,7 @@ int main(int argc, char* argv[])
         cout << "Too few arguments.\n";
         return 0;
     }
-    else if (argc > 3)
+    else if (argc > 4)
     {
         cout << "Too many arguments. Some of them will be discarded.\n";
     }
@@ -33,41 +33,79 @@ int main(int argc, char* argv[])
     }
 
     size_t length = fileSize(sourceFile);
-    unsigned int amount;
+    size_t amount;
+    size_t eachFileSize = 0;
+    size_t n = 0;
     try
     {
         amount = stoi((string)argv[2]);
+        if (amount <= 0)
+        {
+            cout << "Amount must be an integer and bigger than zero.\n";
+            return 0;
+        }
+        eachFileSize = length / amount;
+        n = length - eachFileSize * amount;
     }
     catch (invalid_argument&)
     {
         amount = 0;
+        string mode(argv[2]);
+        if (mode == "-b")
+        {
+            if (argc < 4)
+            {
+                cout << "Too few arguments.\n";
+                return 0;
+            }
+            eachFileSize = stoull((string)argv[3]);
+        }
+        else if (mode == "-k")
+        {
+            if (argc < 4)
+            {
+                cout << "Too few arguments.\n";
+                return 0;
+            }
+            eachFileSize = stoull((string)argv[3]) * 1024;
+        }
+        else if (mode == "-m")
+        {
+            if (argc < 4)
+            {
+                cout << "Too few arguments.\n";
+                return 0;
+            }
+            eachFileSize = stoull((string)argv[3]) * 1024 * 1024;
+        }
+        else
+        {
+            cout << "Mode must be -b, -k or -m.\n";
+            return 0;
+        }
+        amount = length / eachFileSize + 1;
     }
     if (amount > length)
     {
         cout << "Amount too big.\n";
         return 0;
     }
-    if (amount <= 0)
-    {
-        cout << "Amount must be an integer and bigger than zero.\n";
-        return 0;
-    }
-    size_t eachFileSize = length / amount;
-    size_t n = length - eachFileSize * amount;
 
-    for (unsigned int i = 0; i < amount; i++)
+    for (size_t i = 1; i <= amount; i++)
     {
         string newFilename;
         newFilename += argv[1];
-        newFilename += "." + to_string(i + 1);
+        newFilename += "." + to_string(i);
 
         ifstream testFile(newFilename);
         if (!testFile.fail())
         {
-            cout << "This program will overwrite " << newFilename << ", do you want to continue?(Y/N)  ";
+            cout << "This program will overwrite " << newFilename
+                << ", do you want to continue?(Y/N)  ";
             char choice;
             cin >> choice;
-            if (choice == 'N' || choice == 'n') return 0;
+            if (choice == 'N' || choice == 'n')
+                return 0;
         }
 
         cout << "Generating " + newFilename + '\n';
@@ -77,6 +115,10 @@ int main(int argc, char* argv[])
         {
             sizeToRead++;
             n--;
+        }
+        if (sizeToRead * i > length)
+        {
+            sizeToRead = length - eachFileSize * (i - 1);
         }
         char* content = NULL;
         content = new char[sizeToRead];
