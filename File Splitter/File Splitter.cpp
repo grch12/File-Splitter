bbf17f3c97b0ad45fe357d03d3b5f6c3faa08300
grch <grch@DESKTOP-38G2HV1>
2024-03-22 16:09:35 +0800

    使用clipp更优雅地处理参数，其他小改

--- File Splitter/File Splitter.cpp
+++ File Splitter/File Splitter.cpp
@@ -1,148 +1,133 @@
 ﻿#include <fstream>
 #include <iostream>
 #include <string>
+#include <memory>
 
-using namespace std;
+#include "clipp.h"
 
-size_t fileSize(ifstream& file)
+using std::string;
+using namespace clipp;
+
+size_t fileSize(std::ifstream& file)
 {
-    streampos fpos = file.tellg();
-    file.seekg(0, ios_base::end);
-    size_t n = file.tellg();
-    file.seekg(fpos);
-    return n;
+	std::streampos fpos = file.tellg();
+	file.seekg(0, std::ios_base::end);
+	size_t n = file.tellg();
+	file.seekg(fpos);
+	return n;
 }
 
-bool fileExist(string filename)
+bool fileExist(const string& filename)
 {
-    ifstream testFile(filename);
-    bool exist = !testFile.fail();
-    testFile.close();
-    return exist;
+	std::ifstream testFile(filename);
+	bool exist = !testFile.fail();
+	testFile.close();
+	return exist;
 }
 
 int main(int argc, char* argv[])
 {
-    if (argc < 4)
-    {
-        cout << "Too few arguments.\n";
-        return 0;
-    }
-    else if (argc > 4)
-    {
-        cout << "Too many arguments. Some of them will be discarded.\n";
-    }
+	string sourceFileName;
+	size_t amount = 0;
+	size_t eachFileSize;
+	size_t n = 0;
+	size_t size;
 
-    ifstream sourceFile(argv[1], ios::binary);
-    if (sourceFile.fail())
-    {
-        cout << "Could not open the source file.\n";
-        return 0;
-    }
+	size_t unit = 0;
 
-    size_t length = fileSize(sourceFile);
-    size_t amount = 0;
-    size_t eachFileSize;
-    size_t n = 0;
-    string mode(argv[2]);
-    size_t param;
+	// 处理参数
+	auto amountCli = (command("-a"),
+		value("amount", amount));
 
-    // 尝试转为 size_t ，失败则退出
-    try
-    {
-        param = stoull(argv[3]);
-    }
-    catch (invalid_argument&)
-    {
-        cout << "Invalid syntax.\n";
-        return 0;
-    }
-    
-    // 选择模式，不支持则退出
-    if (mode == "-a")
-    {
-        amount = param;
-        eachFileSize = length / amount;
-    }
-    else if (mode == "-b")
-    {
-        eachFileSize = param;
-    }
-    else if (mode == "-k")
-    {
-        eachFileSize = param * 1024;
-    }
-    else if (mode == "-m")
-    {
-        eachFileSize = param * 1024 * 1024;
-    }
-    else
-    {
-        cout << "Mode must be -a, -b, -k or -m.\n";
-        return 0;
-    }
+	auto sizeCli = ((option("-b", "--byte").set(unit, 1llu).doc("use byte as the unit") |
+		option("-k", "--kib").set(unit, 1024llu).doc("use kilobyte as the unit") |
+		option("-m", "--mib").set(unit, 1048576llu).doc("use megabyte as the unit")),
+		value("size", size));
 
-    // 指定大小的模式
-    if(!amount)
-    {
-        amount = length / eachFileSize + 1;
-    }
-    // 指定总数的模式，处理除法产生的“余数”（剩余的字节）
-    else {
-        n = length - amount * eachFileSize;
-    }
+	auto cli = (value("input file", sourceFileName),
+		(amountCli | sizeCli));
 
-    // 总数不能为 0 ，也不能大于文件大小
-    if (amount > length)
-    {
-        cout << "Amount too big.\n";
-        return 0;
-    }
-    if (amount <= 0)
-    {
-        cout << "Amount must be an integer and bigger than 0.\n";
-    }
+	if (!parse(argc, argv, cli))
+	{
+		std::cout << make_man_page(cli, argv[0]);
+		return 0;
+	}
 
-    // for 循环，依次创建每个新文件
-    for (size_t i = 1; i <= amount; i++)
-    {
-        string newFilename(argv[1]);
-        newFilename += "." + to_string(i);
+	std::ifstream sourceFile(sourceFileName);
+	if (sourceFile.fail())
+	{
+		std::cout << "Could not open the source file.\n";
+		return 0;
+	}
+	size_t length = fileSize(sourceFile);
 
-        // 如果同名文件已存在，则询问是否覆盖
-        if (fileExist(newFilename))
-        {
-            cout << "This program will overwrite " << newFilename
-                << ", do you want to continue?(Y/N)  ";
-            char choice;
-            cin >> choice;
-            if (choice == 'N' || choice == 'n')
-                return 0;
-        }
+	if (sourceFile.fail())
+	{
+		std::cout << "Could not open the source file.\n";
+		return 0;
+	}
 
-        cout << "Generating " + newFilename + '\n';
+	// 指定大小的模式
+	if (!amount)
+	{
+		eachFileSize = size * unit;
+		amount = length / eachFileSize + 1;
+	}
+	// 指定总数的模式，处理除法产生的“余数”（剩余的字节）
+	else
+	{
+		// 总数不能为 0 ，也不能大于文件大小
+		if (amount > length)
+		{
+			std::cout << "Amount too big.\n";
+			return 0;
+		}
+		if (amount <= 0)
+		{
+			std::cout << "Amount must be an integer and bigger than 0.\n";
+			return 0;
+		}
+		eachFileSize = length / amount;
+		n = length - amount * eachFileSize;
+	}
 
-        size_t sizeToRead = eachFileSize;
-        if (n > 0)
-        {
-            sizeToRead++;
-            n--;
-        }
-        if (sizeToRead * i > length)
-        {
-            sizeToRead = length - eachFileSize * (i - 1);
-        }
-        char* content = NULL;
-        content = new char[sizeToRead];
-        ofstream newFile(newFilename, ios::binary);
-        sourceFile.read(content, sizeToRead);
-        newFile.write(content, sizeToRead);
-        newFile.close();
-        delete[] content;
-    }
+	// for 循环，依次创建每个新文件
+	for (size_t i = 1; i <= amount; i++)
+	{
+		string newFilename(argv[1]);
+		newFilename += "." + std::to_string(i);
 
+		// 如果同名文件已存在，则询问是否覆盖
+		if (fileExist(newFilename))
+		{
+			std::cout << "This program will overwrite " << newFilename
+				<< ", do you want to continue?(Y/N)  ";
+			char choice;
+			std::cin >> choice;
+			if (choice == 'N' || choice == 'n')
+				return 0;
+		}
 
-    // 关闭源文件
-    sourceFile.close();
-    return 0;
+		std::cout << "Generating " + newFilename + '\n';
+
+		size_t sizeToRead = eachFileSize;
+		if (n > 0)
+		{
+			sizeToRead++;
+			n--;
+		}
+		if (sizeToRead * i > length)
+		{
+			sizeToRead = length - eachFileSize * (i - 1);
+		}
+		std::unique_ptr<char[]> content(new char[sizeToRead]);
+		std::ofstream newFile(newFilename, std::ios::binary);
+		sourceFile.read(content.get(), sizeToRead);
+		newFile.write(content.get(), sizeToRead);
+		newFile.close();
+	}
+
+	// 关闭源文件
+	sourceFile.close();
+	return 0;
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+# File Splitter
+文件分割小程序。
+使用了[clipp](https://github.com/muellan/clipp)用于更优雅地处理命令行参数
